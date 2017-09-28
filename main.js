d7add9307b6548f4b4e786953ca5ee4e85a11021
Miller Richey <miller2502@gmail.com>
2017-09-29 01:21:06 +1000

    Update main.js with role removing. Role adding WIP

--- main.js
+++ main.js
@@ -56,35 +56,3 @@ CLIENT.on("message", async message => {
 
 	}
 }
-
-
-
-
-
-	}
-=======
-const EMOTES = require("./data/teamemotes.json");
-
-CLIENT.on('ready', () => {
-    console.log(`--------------------------------------------------------`)
-    console.log(`Logged in and successfully connected as ${CLIENT.user.username}.`)
-    console.log(`Invite link: https://discordapp.com/oauth2/authorize?CLIENT_id=${CLIENT.user.id}&scope=bot&permissions=268446784`)
-    console.log(`--------------------------------------------------------`);
-    CLIENT.user.setGame("Prefix: " + CONFIG.prefix);
-});
-
-CLIENT.on("message", async message => {
-    const SERVER = CLIENT.guilds.find("name", "r/AFL");
-
-    if(message.author.bot) return;
-
-    if(message.content.indexOf(CONFIG.prefix) !== 0) return;
-
-    const args = message.content.slice(CONFIG.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    if(command==="team") {
-        
-    }
->>>>>>> 2127522359b588fddfaa5b65fdad51a1f6a80899
-}
