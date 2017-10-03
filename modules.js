cd7eaf597bc4e6d0109842de406091b0d6af747c
Cameron Regan <cameronthomasregan@gmail.com>
2017-10-03 21:42:29 +1100

    More work on modules.js, but moving to other system

--- modules.js
+++ modules.js
@@ -1,5 +1,18 @@
-var ScoreBot = function() {
+const open = require("open-uri");
 
+var ScoreBot = function() {
+	function getID() {
+		var games = open("http://dtlive.com.au/afl/viewgames.php")
+		var in_progress = 
+	};
+
+	function getInProgressGames () {
+
+	};
+
+	function processFeed () {
+
+	};
 }();
 
 var DraftBot = function() {
